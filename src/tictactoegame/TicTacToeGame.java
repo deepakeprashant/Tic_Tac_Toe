de4c7d983e4f11b4d521dd3e8cf335b466577bed
Prashant Deepake <deepakeprashant1@gmail.com>
2023-01-10 20:35:34 +0530

    ADDED : PLAYER CHOOSE LETTER X OR O

--- src/tictactoegame/TicTacToeGame.java
+++ src/tictactoegame/TicTacToeGame.java
@@ -1,20 +1,43 @@
 package tictactoegame;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
     char[] board = new char[9];
+    char userChoiceLetter;
+    char cpuAllocateLetter;
+    Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("::------- WELCOME TO TIC TAC TOE GAME -------::");
         TicTacToeGame mainObject = new TicTacToeGame();
         mainObject.createBoard();
         mainObject.showBoard();
+        mainObject.choiceLetter();
     }
-    void createBoard(){
+
+    void createBoard() {
         int length = board.length;
         for (int i = 0; i < length; i++) {
             board[i] = 'N';
         }
     }
-    void showBoard(){
+
+    void choiceLetter() {
+        System.out.println("\nPLEASE CHOICE ONE LETTER \n ENTER 1] CHOOSE 'X'\n ENTER 2] CHOOSE 'O'");
+        int choice = scan.nextInt();
+        if (choice == 1){
+            userChoiceLetter = 'X';
+            cpuAllocateLetter = 'O';
+            System.out.println("USER CHOOSE LETTER : "+userChoiceLetter);
+            return;
+        }
+        userChoiceLetter = '0';
+        cpuAllocateLetter = 'X';
+        System.out.println("USER CHOOSE LETTER : "+userChoiceLetter);
+    }
+
+    void showBoard() {
         System.out.println("|---|---|---|");
         System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
         System.out.println("|-----------|");
